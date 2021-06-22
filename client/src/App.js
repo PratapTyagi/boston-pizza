9ff280bcad9e66bfd460ad65f9d58333aa5901fe
Pratap Tyagi <tyagipratap111@gmail.com>
2021-06-22 14:55:04 +0530

    App start with login screen

--- client/src/App.js
+++ client/src/App.js
@@ -6,28 +6,40 @@ import Login from "./screens/authentication/Login";
 import Register from "./screens/authentication/Register";
 import OrdersScreen from "./screens/orders/OrdersScreen";
 import AdminScreen from "./screens/admin/AdminScreen";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import "./App.css";
-function App() {
+import { useEffect } from "react";
+
+const Routing = () => {
   const { currentUser } = useSelector((state) => state.loginUserReducer);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!currentUser) {
+      history.push("/login");
+    }
+  }, []);
+
+  return (
+    <>
+      <Route path="/" exact component={Home} />
+      <Route path="/cart" exact component={Cart} />
+      <Route path="/login" exact component={Login} />
+      <Route path="/register" exact component={Register} />
+      <Route path="/orders" exact component={OrdersScreen} />
+      <Route path="/admin" component={AdminScreen} />
+    </>
+  );
+};
+
+function App() {
   return (
     <div className="app">
       <Router>
         <Navbar />
-        {currentUser ? (
-          <>
-            <Route path="/" exact component={Home} />
-            <Route path="/cart" exact component={Cart} />
-            <Route path="/login" exact component={Login} />
-            <Route path="/register" exact component={Register} />
-            <Route path="/orders" exact component={OrdersScreen} />
-            <Route path="/admin" component={AdminScreen} />
-          </>
-        ) : (
-          <Route path="/login" exact component={Login} />
-        )}
+        <Routing />
       </Router>
     </div>
   );
