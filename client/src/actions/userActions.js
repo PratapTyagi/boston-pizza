6df0d1057c9949632ee07d54a1878017748a55d3
Pratap Tyagi <tyagipratap111@gmail.com>
2021-06-25 13:44:17 +0530

    Resolved local storage error while logout

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -24,7 +24,7 @@ export const loginUser = (user) => async (dispatch) => {
 };
 
 export const logoutUser = () => (dispatch) => {
-  localStorage.removeItem("currentUser");
+  localStorage.clear();
   window.location.href = "/";
 };
 
