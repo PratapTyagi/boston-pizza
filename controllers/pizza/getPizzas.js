cab7eab772d4afdb05631ed237ba9d9f68c4578a
PratapTyagi <tyagipratap111@gmail.com>
2021-10-01 15:21:22 +0530

    Improved filter feature backend

--- controllers/pizza/getPizzas.js
+++ controllers/pizza/getPizzas.js
@@ -5,6 +5,30 @@ export const getPizzas = async (req, res) => {
     const pizzas = await Model.find({});
     res.send(pizzas);
   } catch (error) {
-    res.status(400).json({ message: error });
+    res.status(400).json(error );
   }
 };
+
+export const filterPizza = async (req, res) => {
+  const {category, query} = req.body;
+  let userPattern = new RegExp(`^${query}`);
+try {
+  let pizzas;
+  if (!category && query) {
+    pizzas = await Model.find({ name: { $regex: userPattern } })
+  }
+  else if (!query && category && category !== "all") {
+    pizzas = await Model.find({category});
+  }
+  else if(query && category) {
+    pizzas = await Model.find({ name: { $regex: userPattern }, category })
+  }
+  else {
+    pizzas = await Model.find();
+  }
+  return res.status(200).json(pizzas)
+} catch (error) {
+  console.log(error);
+  return res.status(400).json(error)
+}
+}
--- routes/pizzaRoute.js
+++ routes/pizzaRoute.js
@@ -6,11 +6,13 @@ import { postPizza } from "../controllers/pizza/postPizza.js";
 import { getPizzaById } from "../controllers/pizza/getPizzaById.js";
 import { editPizza } from "../controllers/pizza/editPizza.js";
 import { deletePizza } from "../controllers/pizza/deletePizza.js";
+import { filterPizza } from "../controllers/pizza/getPizzas.js";
 
 router.get("/getpizzas", getPizzas);
 router.post("/addpizzas", postPizza);
 router.post("/pizzabyid", getPizzaById);
 router.post("/editpizza", editPizza);
 router.post("/deletepizza", deletePizza);
+router.post("/filter-pizza", filterPizza);
 
 export default router;
